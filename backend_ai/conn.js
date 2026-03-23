2e75e2c7336aae527e0c8bdb50ba2db3b65a3f52
sonalisamadhiya <samadhiyasonali32@gmail.com>
2026-03-23 12:04:17 -0400

    Update conn.js

--- backend_ai/conn.js
+++ backend_ai/conn.js
@@ -1,7 +1,7 @@
 const mongoose =require('mongoose');
 
 
-mongoose.connect('mongodb+srv://samadhiyasonali32_db_user:LxAFlB6Wg8gDWMHH@cluster0.0otbamq.mongodb.net/?appName=Cluster0').then((res)=>{
+mongoose.connect('mongo Atlas link').then((res)=>{
   console.log("Database connected Successfully");
 }).catch((err)=>{
   console.log("Something error",err);
@@ -9,10 +9,3 @@ mongoose.connect('mongodb+srv://samadhiyasonali32_db_user:LxAFlB6Wg8gDWMHH@clust
 
 
 
-
-
-// LxAFlB6Wg8gDWMHH
-
-// samadhiyasonali32_db_user
-
-// mongodb+srv://samadhiyasonali32_db_user:LxAFlB6Wg8gDWMHH@cluster0.0otbamq.mongodb.net/?appName=Cluster0
